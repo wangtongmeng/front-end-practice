db0ff62e116e1c7ac4a59d3978a452e657774d83
littlebirdflying <tongmengwang@163.com>
2018-10-01 23:03:33 +0800

    demo save

---
+++ javascript-datastructures-algorithms/1-01-Stack.js
@@ -0,0 +1,47 @@
+function Stack() {
+
+    let items = [];
+
+    this.push = function(element){
+        items.push(element);
+    };
+
+    this.pop = function(){
+        return items.pop();
+    };
+
+    this.peek = function(){
+        return items[items.length-1];
+    };
+
+    this.isEmpty = function(){
+        return items.length == 0;
+    };
+
+    this.size = function(){
+        return items.length;
+    };
+
+    this.clear = function(){
+        items = [];
+    };
+
+    this.print = function(){
+        console.log(items.toString());
+    };
+
+    this.toString = function(){
+        return items.toString();
+    };
+}
+
+let stack = new Stack()
+let stack1 = new Stack()
+console.log(stack);
+console.log(stack1);
+stack.push(1)
+console.log(stack.size());
+console.log(stack1.size());
+
+
+
---
+++ javascript-datastructures-algorithms/LearningJavaScriptDataStructuresandAlgorithmsSecondEdition_Code/README.txt
@@ -0,0 +1,12 @@
+If you are using Mozilla Firefox in order to run the code files, make sure you download and install Firebug plugin.
+
+You can use either of the environment mentioned in Chapter 1, in order to run the code files.
+
+Chapter 3 : You will find Tower of Hanoi code file in the chapter03 folder.
+
+Chapter 8 : You will find Red black tree code file in the chapter08 folder.
+
+Chapter 11 : You will find Knapsack Recursive, Recursive Longest Common Subsequence Recursive,                           Matrix Chain Multiplication Recursive in the chapter11 folder.
+
+Chapter 12 : Big O chart is plotted using javascript, you can find the code file inside the big0chart folder              under chapter12. 
+
