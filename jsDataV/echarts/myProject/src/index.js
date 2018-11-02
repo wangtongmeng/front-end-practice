71f5d181788519ef6a695b6806548fc72933590f
littlebirdflying <tongmengwang@163.com>
2018-11-02 17:40:43 +0800

    webpack 管理输出章节

--- jsDataV/echarts/myProject/src/index.js
+++ jsDataV/echarts/myProject/src/index.js
@@ -1,22 +1,11 @@
 import _ from "lodash";
-import "./style.css";
-import bird from "./bird.jpg";
-import Data from './data.xml';
 
 function component() {
     var element = document.createElement("div");
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(["Hello", "webpack"], " ");
-    element.classList.add("hello");
 
-    // 将图像添加到我们现有的 div。
-    var myIcon = new Image();
-    myIcon.src = bird;
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    
     return element;
 }
 
--- jsDataV/echarts/myProject/webpack.config.js
+++ jsDataV/echarts/myProject/webpack.config.js
@@ -6,24 +6,5 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ["file-loader"]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: ["csv-loader"]
-            },
-            {
-                test: /\.xml$/,
-                use: ["xml-loader"]
-            }
-        ]
-    }
+    
 };
