b431a255c728cebcd8aa3dcb246b7a30040c56e9
littlebirdflying <tongmengwang@163.com>
2019-02-26 11:15:21 +0800

    upload

--- vue.js/vue项目构建开发与入门/my-project/src/main.js
+++ vue.js/vue项目构建开发与入门/my-project/src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+console.log(process.env)
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
--- vue.js/vue项目构建开发与入门/my-project/vue.config.js
+++ vue.js/vue项目构建开发与入门/my-project/vue.config.js
@@ -1,4 +1,11 @@
+const configs = require('./config')
+
+// 用于做相应的 merge 处理
 const merge = require('webpack-merge');
+
+// 根据环境判断使用哪份配置
+const cfg = process.env.NODE_ENV === 'production' ? configs.build.env : configs.dev.env
+
 
 module.exports = {
     publicPath: 'vue',
@@ -15,6 +22,15 @@ module.exports = {
                     limit: 5120,
                 })
             )
+        config.plugin('define')
+        .tap(args => {
+            let name = 'process.env';
+            
+            // 使用 merge 保证原始值不变
+            args[0][name] = merge(args[0][name], cfg);
+
+            return args
+        })
     },
     // config 参数为已经解析好的 webpack 配置
     configureWebpack: config => {
