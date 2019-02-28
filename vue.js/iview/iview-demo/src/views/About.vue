689caf1a16c20021531f089090a93d30c64fd7ee
littlebirdflying <tongmengwang@163.com>
2019-02-28 17:15:51 +0800

    upload

---
+++ vue.js/iview/iview-demo/src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <Menu></Menu>
+  </div>
+</template>
+<script>
+import Menu from "../components/Menu"
+export default {
+    components: {
+        Menu
+    }
+}
+</script>
+
