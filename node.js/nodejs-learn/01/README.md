057c356060e5bac2995b6fb55ff07ea58ae22821
littlebirdflying <tongmengwang@163.com>
2019-03-01 08:16:47 +0800

    upload

---
+++ node.js/nodejs-learn/01/README.md
@@ -0,0 +1,13 @@
+# Node.js 第1天
+
+## 上午总结
+
+- Node.js 是什么
+  + JavaScript 运行时
+  + 既不是语言，也不是框架，它是一个平台
+- Node.js 中的 JavaScript
+  + 没有 BOM、DOM
+  + EcmaScript
+  + 在 Node 中为 JavaScript 提供了一些服务器级别的 API
+    * 文件操作
+    * http服务
--- node.js/nodejs-learn/01/code/06-http-res.js
+++ node.js/nodejs-learn/01/code/06-http-res.js
@@ -14,7 +14,27 @@ server.on('request', function (request, response) {
   console.log('收到客户端请求，请求路径时：' + request.url)
 
   // response 对象有一个方法：write 可以用来给客户端发送响应数据
-  // write 可以使用多次，但最后必须使用 end 来结束响应，否则客户端会一直等待
+  // write 可以使用多次，但最后必须使用 end 来结束响应，否则客户端会一直等待 
+
+  // 当请求不同路径时，响应不同的结果
+  // 如：
+  // / index
+  // /login 登录
+  // /register 注册
+  // /haha 哈哈哈
+  if (request.url === '/') {
+    response.write('index')
+  } else if (request.url === '/login') {
+    response.write('登录')
+  } else if (request.url === '/reqister') {
+    response.write('注册')
+  } else if (request.url === '/haha') {
+    response.write('哈哈哈')
+  } else {
+    response.write('没有找到页面')
+  }
+
+  response.end()
 })
 
 server.listen(3000, function () {
