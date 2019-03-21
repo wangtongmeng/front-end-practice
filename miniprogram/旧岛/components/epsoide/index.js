eb6edc3f4fe7e4196b0e0ac4d8484652822e25f7
littlebirdflying <tongmengwang@163.com>
2019-03-21 08:13:18 +0800

    upload

--- miniprogram/旧岛/components/epsoide/index.js
+++ miniprogram/旧岛/components/epsoide/index.js
@@ -4,20 +4,36 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    index: Number
+    index: {
+      type: String,
+      observer: function (newVal, oldVal, changedPath) {
+        let val = newVal < 10 ? '0' + newVal : newVal
+        this.setData({
+          _index: val
+        })
+      }
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    months: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
     year: Number,
-    month: String
+    month: String,
+    _index: ''
   },
 
-  attached: function(){
-    console.log(this.properties)
-    console.log(this.data)
+  attached: function () {
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
+
+    this.setData({
+      year: year,
+      month: this.data.months[month]
+    })
   },
   /**
    * 组件的方法列表
