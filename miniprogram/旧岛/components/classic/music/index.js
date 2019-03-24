3aaa8ecb767ca01a84be443545731c05abc3ee82
littlebirdflying <tongmengwang@163.com>
2019-03-24 23:47:19 +0800

    upload

--- miniprogram/旧岛/components/classic/music/index.js
+++ miniprogram/旧岛/components/classic/music/index.js
@@ -1,25 +1,38 @@
 import {classicBeh} from '../class-beh.js'
 
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
    */
-  behavior: [classicBeh],
+  behaviors: [classicBeh],
   properties: {
+    src: String,
+    title: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    pauseSrc: 'images/player@waitting.png',
-    playSrc: 'images/player@playing.png'
+    playing: false,
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay: function(event) {
+      this.setData({
+        playing: true
+      })
+      mMgr.title = this.properties.title
+      mMgr.src = this.properties.src
+      console.log(this.data.musicSrc)
+      
+    }
   }
 })
--- miniprogram/旧岛/pages/classic/classic.js
+++ miniprogram/旧岛/pages/classic/classic.js
@@ -1,5 +1,9 @@
-import { ClassicModel } from '../../models/classic.js'
-import { LikeModel } from '../../models/like.js'
+import {
+  ClassicModel
+} from '../../models/classic.js'
+import {
+  LikeModel
+} from '../../models/like.js'
 
 let classicModel = new ClassicModel()
 let likeModel = new LikeModel()
@@ -23,9 +27,11 @@ Page({
   onLoad: function (options) {
     // 通过 Storage 存储最新一期期刊的 index
     classicModel.getLatest((res) => {
-      this._getLikeStatus(res.id, res.type)
+      // this._getLikeStatus(res.id, res.type)
       this.setData({
         classic: res,
+        likeCount: res.fav_nums,
+        likeStatus: res.like_status
       })
     })
     // lastestClassic latestIndex currentClassic currentIndex，从而判断是否是最新一期期刊
@@ -44,26 +50,26 @@ Page({
     this._updateClassic('previous')
   },
 
-  _updateClassic: function(nextOrPrevious) {
+  _updateClassic: function (nextOrPrevious) {
     let index = this.data.classic.index
     classicModel.getClassic(index, nextOrPrevious, (res) => {
-    this._getLikeStatus(res.id, res.type)
-        this.setData({
-            classic: res,
-            latest: classicModel.isLatest(res.index),
-            first: classicModel.isFirst(res.index)
-        })
+      // this._getLikeStatus(res.id, res.type)
+      this.setData({
+        classic: res,
+        latest: classicModel.isLatest(res.index),
+        first: classicModel.isFirst(res.index)
+      })
     })
   },
 
-  _getLikeStatus: function(artID, category) {
-    likeModel.getClassicLikeStatus(artID, category, (res)=>{
-        this.setData({
-            likeCount: res.fav_nums,
-            likeStatus: res.like_status
-        })
+  _getLikeStatus: function (artID, category) {
+    likeModel.getClassicLikeStatus(artID, category, (res) => {
+      this.setData({
+        likeCount: res.fav_nums,
+        likeStatus: res.like_status
+      })
     })
   }
 
-  
-}) 
+
+})
