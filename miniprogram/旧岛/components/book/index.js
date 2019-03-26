69f04feafe5e7311893100b08533e6e1511c547a
littlebirdflying <tongmengwang@163.com>
2019-03-26 16:31:51 +0800

    upload

--- miniprogram/旧岛/components/book/index.js
+++ miniprogram/旧岛/components/book/index.js
@@ -18,6 +18,11 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onTap: function (event) {
+      const bid = this.properties.book.id
+      wx.navigateTo({
+        url: `/pages/book-detail/book-detail?bid=${bid}`
+      })
+    }
   }
 })
