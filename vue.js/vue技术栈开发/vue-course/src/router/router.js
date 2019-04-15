600250daf416cafed6c495ce6a88d0eceb434fbe
littlebirdflying <tongmengwang@163.com>
2019-04-15 17:42:34 +0800

    upload

--- vue.js/vue技术栈开发/vue-course/src/router/router.js
+++ vue.js/vue技术栈开发/vue-course/src/router/router.js
@@ -1,4 +1,4 @@
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 export default [
 	{
@@ -8,10 +8,13 @@ export default [
 	},
 	{
 		path: '/about',
-		name: 'about',
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+		component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+	},
+	{
+		path: '/argu/:name',
+		component: () => import('@/views/argu.vue')
 	}
 ]
---
+++ vue.js/vue技术栈开发/vue-course/src/views/argu.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		{{ $route.params.name }}
+	</div>
+</template>
+<script>
+export default {
+	//
+}
+</script>
