1109a8c5a83b9dd18b5c53bb7b077cec7a04def4
littlebirdflying <tongmengwang@163.com>
2019-04-15 17:21:09 +0800

    vue-技术栈开发-创建项目-补充修改

--- vue.js/vue技术栈开发/vue-course/vue.config.js
+++ vue.js/vue技术栈开发/vue-course/vue.config.js
@@ -1,3 +1,20 @@
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/iview-admin' : '/'
+
 module.exports = {
-  lintOnSave: false
+	lintOnSave: false,
+	baseUrl: BASE_URL,
+	chainWebpack: config => {
+		config.resolve.alias
+			.set('@', resolve('src'))
+			.set('_c', resolve('src/components'))
+	},
+	// 打包时不生成.map文件，减少体积，加快打包速度 
+	productionSourceMap: false,
+	devServer: {
+		proxy: 'http://localhost:4000'
+	}
 }
