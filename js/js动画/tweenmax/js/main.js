fafda706c4cb91fba664bdf6fa2d255ea47a7a2e
littlebirdflying <tongmengwang@163.com>
2019-05-05 17:35:58 +0800

    upload

--- js/js动画/tweenmax/js/main.js
+++ js/js动画/tweenmax/js/main.js
@@ -1,4 +1,16 @@
-new TweenMax('.box', 3, {
+var progressBtn = document.getElementById('progress_btn')
+var totalProgress = document.getElementById('total_progress')
+
+var myTween=new TweenMax('.box', 3, {
   x: 500,
-  ease: Bounce.easeIn
+  repeat: 3,
+  repeatDelay:2, 
+  onUpdate: function () {
+    totalProgress.innerText = this.progress().toFixed(2)
+  }
 });
+
+
+progressBtn.addEventListener('click', function () {
+  myTween.progress( 0.25 )
+})
