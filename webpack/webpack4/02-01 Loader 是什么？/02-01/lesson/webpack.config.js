2c85c330514c97e3bc42ba1520ffd875c9955706
littlebirdflying <tongmengwang@163.com>
2019-05-28 17:49:59 +0800

    upload

---
+++ webpack/webpack4/02-01 Loader 是什么？/02-01/lesson/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/index.js'
+	},
+	module: {
+		rules: [{
+			test: /\.jpg$/,
+			use: {
+				loader: 'file-loader'
+			} 
+		}]
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+}
