5fd529d84de17f3bad6feb7f06477cb8e9f5b8d0
littlebirdflying <tongmengwang@163.com>
2019-05-31 11:44:06 +0800

    Vue技术栈开发实战-Tree组件实现文件目录1

--- vue.js/vue技术栈开发/vue-course/src/views/folder-tree/folder-tree.vue
+++ vue.js/vue技术栈开发/vue-course/src/views/folder-tree/folder-tree.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="folder-wrapper">
-		
+		<Tree :data="folderTree" :render="renderFunc"></Tree>
 	</div>
 </template>
 <script>
@@ -10,18 +10,33 @@ export default {
 	data () {
 		return {
 			folderList: [],
-			fileList: []
+      fileList: [],
+      folderTree: [],
+      renderFunc: (h, { root, node, data}) => {
+        return (
+					<div class="tree-item">
+						{ data.type === 'folder' ? <icon type="ios-folder" color="#2d8cf0" style="margin-right: 10px;"/> : ''}
+            { data.title }
+					</div>
+        )
+      }
 		}
 	},
 	mounted () {
 		Promise.all([getFolderList(), getFileList()]).then(res => {
-			console.log(transferFolderToTree(putFileInFolder(res[0], res[1])))
+			this.folderTree = transferFolderToTree(putFileInFolder(res[0], res[1]))
 		})
 	}
 }
 </script>
-<style scoped>
+<style lang="less">
 .folder-wrapper {
 	width: 300px;
+  .tree-item {
+    display: inline-block;
+		width: ~"calc(100% - 50px)";
+		height: 30px;
+    line-height: 30px;
+  }
 }
 </style>
