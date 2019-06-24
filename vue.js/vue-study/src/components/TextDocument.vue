82e9a0a7fc4549d98a6fd15069239bc8d487067e
王童孟 <tongmengwang@163.com>
2019-06-24 17:35:48 +0800

    upload

---
+++ vue.js/vue-study/src/components/TextDocument.vue
@@ -0,0 +1,18 @@
+<template>
+  <div @click="handleClick">{{title}}</div>
+</template>
+<script>
+export default {
+  props: {
+    title: String
+  },
+  methods: {
+    handleClick () {
+      this.$emit('update:title', '新title')
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
---
+++ vue.js/vue-study/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <text-document
+      :title.sync="doc.title"
+    ></text-document>
+  </div>
+</template>
+
+<script>
+import TextDocument from '@/components/TextDocument.vue'
+export default {
+  name: 'home',
+  components: {
+    TextDocument
+  },
+  data () {
+    return {
+      doc: {
+        title: '文档标题'
+      }
+    }
+  }
+}
+</script>
