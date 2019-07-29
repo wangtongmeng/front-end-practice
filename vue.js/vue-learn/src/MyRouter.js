3c85a1969d29e7da965ac78119cb4c1667b7860d
王童孟 <tongmengwang@163.com>
2019-07-29 19:29:42 +0800

    upload

---
+++ vue.js/vue-learn/src/MyRouter.js
@@ -0,0 +1,76 @@
+import Vue from 'vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+class VueRouter {
+
+    constructor(options) {
+        this.$options = options
+        this.routeMap = {}
+
+        // 路由响应式，利用了Vue的机制    vue-router的特点，它和react-router的区别：vue-router和vue强绑定关系，只能用在vue中
+        this.app = new Vue({
+            data: {
+                current: '/'
+            }
+        })
+    }
+
+    init() {
+        this.bindEvents() // 监听url变化
+        this.createRouteMap(this.$options) // 解析路由配置
+        this.initComponent() // 实现两个组件
+    }
+
+    bindEvents() {
+        window.addEventListener('load', this.onHashChange.bind(this))
+        window.addEventListener('hashchange', this.onHashChange.bind(this))
+    }
+    onHashChange() {
+        this.app.current = window.location.hash.slice(1) || '/'
+    }
+    createRouteMap(options) {
+        options.routes.forEach(item => {
+            this.routeMap[item.path] = item.component
+        })
+    }
+    initComponent() {
+        // router-link, router-view
+        // <router-link to="">xxx</router-link>
+        Vue.component('router-link', {
+            props: { to: String },
+            render(h) {
+                // h(tag, data, children)
+                return h('a', {
+                    attrs: { href: '#' + this.to }
+                }, [
+                    this.$slots.default
+                ])
+            }
+        })
+    }
+
+}
+
+VueRouter.install = function(Vue) {
+    // 混入，常用于组件的扩展
+    Vue.mixin({
+        beforeCreate() {
+            // this是vue实例
+            if (this.$options.router) {
+                // 仅在根组件执行一次
+                Vue.prototype.$router = this.$options.router
+                this.$options.router.init()
+            }
+        }
+    })
+}
+
+Vue.use(VueRouter)
+ 
+export default new VueRouter({
+    routes: [
+        { path: '/', component: Home },
+        { path: '/about', component: About }
+    ]
+})
