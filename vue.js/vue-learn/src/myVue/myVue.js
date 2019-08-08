3cb125d2dba78932976a1f435b8efb64c8ed3a9a
王童孟 <tongmengwang@163.com>
2019-08-08 18:16:36 +0800

    upload

--- vue.js/vue-learn/src/myVue/myVue.js
+++ vue.js/vue-learn/src/myVue/myVue.js
@@ -21,10 +21,27 @@ class MyVue {
     Object.keys(value).forEach(key => {
       // 对key做响应式处理
       this.defineReactive(value, key, value[key])
+      this.proxyData(key)
+    })
+  }
+
+  // 在vue根上定义属性代理data中的数据
+  proxyData(key) {
+    Object.defineProperty(this, key, {
+      get() {
+        return this.$data[key]
+      },
+      set(newVal) {
+        this.$data[key] = newVal
+      }
     })
   }
 
   defineReactive(obj, key, val) {
+
+    // 递归
+    this.observe(val)
+    // 给obj定义属性
     Object.defineProperty(obj, key, {
         get() {
           return val
