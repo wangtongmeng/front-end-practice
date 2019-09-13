46b275e994a3180189f046362db55eceb37d0e0f
littlebirdflying <tongmengwang@163.com>
2019-09-13 15:55:27 +0800

    upload

--- React.js/react-study/src/index.js
+++ React.js/react-study/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import logo from './logo.svg'
+import './index.css'
+import style from './index.module.css' // 用模块化的方式导入
 
 // React 类负责逻辑控制，比如修改数据 -> vdom
 // ReactDom 类负责渲染，vdom -> dom
@@ -14,13 +17,28 @@ function formatName(user) {
 const greet = <p>hello,jerry</p>
 // 由于条件语句或者循环语句不是合法表达式
 // const title = name ? <h1>{name}</h1> : null
+
+// 数组会作为一组子元素对待
+const arr = [1, 2, 3]
+const arr1 = [1, 2, 3].map(num => <li key={num}>{num}</li>)
+
 const jsx = (
-	<div>
+    <div>
+    {/*条件语句*/}
      {name ? <h1>{name}</h1> : null}
     {/* 函数也是合法表达式 */}
     <p>{formatName(user)}</p>
     {/* jsx 本身也是合法表达式 */}
     {greet}
+    {/* 数组处理 */}
+    {arr}
+    {/* 显示列表 */}
+    <ul>{arr1}</ul>
+    {/* 属性使用：静态值用双引号，动态值用花括号 */}
+    {/* class、for关键字要特殊处理，因为jsx最终都会转换成js对象 */}
+    <img src={logo} alt="logo" style={{width: 100}} className="img" /> {/* class="img" */}
+    <img src={logo} alt="logo" style={{width: 100}} className={style.img} /> {/* class="src_img__2a8ZU" 优先级 css>module css > sass */} 
+    {/* <label htmlFor=""></label> */}
 	</div>
 )
 console.log(jsx)
