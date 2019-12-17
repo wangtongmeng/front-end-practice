d66743f45b1dc802f5ed29d9aa93a77b8843157a
王童孟 <tongmengwang@163.com>
2019-12-17 10:56:20 +0800

    remove some page

--- vue.js/vue-practice/src/router/index.js
+++ vue.js/vue-practice/src/router/index.js
@@ -11,11 +11,6 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('@views/About.vue')
-  },
-  {
     path: '/v-chart',
     name: 'v-chart',
     component: () => import('@views/v-chart/VChart.vue')
