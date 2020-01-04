8451770bd78d231a1d0f9c5fb9732c2020b0716d
littlebirdflying <tongmengwang@163.com>
2020-01-04 09:16:51 +0800

    upload

---
+++ node.js/nodejs-blog/commonjs-test/a.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a + b
+}
+
+function mul(a, b) {
+  return a * b
+}
+
+// module.exports = add
+module.exports = {
+  add,
+  mul
+}
---
+++ node.js/nodejs-blog/commonjs-test/b.js
@@ -0,0 +1,7 @@
+// const add = require('./a')
+const { add, mul } = require('./a')
+const sum = add(10, 20)
+const result = mul(100, 200)
+
+console.log(sum)
+console.log(result)
