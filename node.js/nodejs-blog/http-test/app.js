ddbba87db10f5eb3cea8ff078c8271f9601583a1
littlebirdflying <tongmengwang@163.com>
2020-01-05 23:30:35 +0800

    upload

--- node.js/nodejs-blog/http-test/app.js
+++ node.js/nodejs-blog/http-test/app.js
@@ -1,31 +1,40 @@
 const http = require('http')
-// const querystring = require('querystring')
-
-// const server = http.createServer((req, res) => {
-//   console.log('method: ', req.method)
-//   const url = req.url
-//   console.log('url: ', url)
-//   req.query = querystring.parse(url.split('?')[1])
-//   console.log('query: ', req.query)
-//   res.end(
-//     JSON.stringify(req.query)
-//   )
-// })
-// server.listen(8000)
-// console.log('server is running on 8000')
+const querystring = require('querystring')
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    // req 数据格式
-    console.log('req content-type', req.headers['content-type'])
-    // 接收数据
+  const method = req.method
+  const url = req.url
+  const path = url.split('?')[0]
+  const query = querystring.parse(url.split('?')[1])
+
+  // 设置返回格式为 JSON
+  res.setHeader('Content-type', 'application/json')
+
+  // 返回的数据
+  const resData = {
+    method,
+    url,
+    path,
+    query
+  }
+
+  // 返回
+  if (method === 'GET') {
+    res.end(
+      JSON.stringify(resData)
+    )
+  }
+  if (method === 'POST') {
     let postData = ''
     req.on('data', chunk => {
       postData += chunk.toString()
     })
     req.on('end', () => {
-      console.log('postData: ', postData)
-      res.end('hello world!')
+      resData.postData = postData
+      // 返回
+      res.end(
+        JSON.stringify(resData)
+      )
     })
   }
 })
