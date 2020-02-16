45ec21cbf7b1fda48fa23e96fa318215814293e3
wangtongmeng <tongmengwang@163.com>
2020-02-16 13:58:58 +0800

    upload

---
+++ React.js/create-react-app-eject版本/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import qs from 'qs';
+import './static/less/index.less';
+
+console.log(qs.parse('name=zhangsan&age=20'))
+
+console.log(1)
