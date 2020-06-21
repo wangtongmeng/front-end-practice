2cebd5271ddec1d96e186c57244c9b5af9f88d4e
wangtongmeng <tongmengwang@163.com>
2020-06-21 20:29:16 +0800

    up

--- webpack/webpack4/01-01 webpack 究竟是什么？/01-01/lesson/sidebar.js
+++
@@ -1,6 +0,0 @@
-function Sidebar() {
-	var dom = document.getElementById('root');
-	var sidebar = document.createElement('div');
-	sidebar.innerText = 'sidebar';
-	dom.append(sidebar);
-}
--- webpack/webpack4/02-01 Loader 是什么？/02-01/lesson/src/index.js
+++
@@ -1,7 +0,0 @@
-import avatar from './avatar.jpg';
-
-var img = new Image();
-img.src = avatar;
-
-var root = document.getElementById('root');
-root.append(img);
