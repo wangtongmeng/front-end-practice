345a8b548b5bd2fa48d2deaedcec91dab02e73b0
wangtongmeng <tongmengwang@163.com>
2020-08-06 20:29:43 +0800

    up

---
+++ React.js/react-init/src/my-react-redux.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ThemeContext = React.createContext()
+export class Provider extends React.Component {
+  static propTypes = {
+    store: PropTypes.object.isRequired
+  }
+  render() {
+    return <ThemeContext.Provider value={{
+      store: this.props.store
+    }}>
+      {this.props.children}
+    </ThemeContext.Provider>
+  }
+}
+
+export function connect(mapStateToProps, mapDispatchToProps) {
+  if (typeof mapStateToProps !== 'function') {
+    mapStateToProps = function () {
+      return {}
+    }
+  }
+  if (typeof mapDispatchToProps !== 'function') {
+    if (mapDispatchToProps !== null && typeof mapDispatchToProps === 'object') {
+
+    } else {
+      mapDispatchToProps = function () {
+        return {}
+      }
+    }
+  }
+
+  return function connectHOC(Component) {
+    return class Proxy extends React.Component {
+      static contextType = ThemeContext
+      render() {
+        return <Component {...this.queryProps()} />
+      }
+      queryProps = () => {
+        const store = this.context.store
+        const state = mapStateToProps(store.getState())
+        const action = mapDispatchToProps(store.dispatch)
+        return {
+          ...state,
+          ...action
+        }
+      }
+      componentDidMount() {
+        this.context.store.subscribe(() => {
+          this.forceUpdate()
+        })
+      }
+    }
+  }
+
+}
+
+// export default connect(state=>state.vote, mapDispatchToProps)(VoteMain)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Provider
+ * connect
+ */
