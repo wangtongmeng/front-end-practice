cef5d08eaa322be5340ab5eb60a70e4a47717a25
wangtongmeng <tongmengwang@163.com>
2020-08-17 12:23:02 +0800

    up

--- React.js/react-init/src/App.jsx
+++ React.js/react-init/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.less'
-import { PageHeader, Button, Tag, Table } from 'antd'
+import { PageHeader, Button, Tag, Table, Modal, message, Input, DatePicker } from 'antd'
+const { confirm } = Modal
+const { TextArea } = Input;
 
 class App extends React.Component {
   state = {
@@ -42,7 +44,9 @@ class App extends React.Component {
     data: [
       { id: 1, task: '任务1任务1任务1任务1任务1任务1任务1任务1任务1任务1任务1任务1任务1任务1任务1任务1任务1', state: 1, time: '2020-08-11 19:55', complete: '2020-08-11 19:55' },
       { id: 2, task: '任务2', state: 2, time: '2020-08-11 19:55', complete: '2020-08-11 19:55' },
-    ]
+    ],
+    // 控制模态框和表单内容
+    visible: false
   }
   addZero = val => {
     val = parseInt(val)
@@ -56,9 +60,15 @@ class App extends React.Component {
     time = time.match(/\d+/g)
     return `${this.addZero(time[1])}-${this.addZero(time[2])} ${this.addZero(time[3])}:${this.addZero(time[4])}:${this.addZero(time[5])}`
   }
+  // 处理模态框
+  handleOK = () => {
 
+  }
+  handleCancel = () => {
+
+  }
   render() {
-    let { columns, data } = this.state
+    let { columns, data, visible } = this.state
     return <div className="container">
       <PageHeader title="任务管理系统">
         <Button type="dashed">新增按钮</Button>
@@ -69,6 +79,20 @@ class App extends React.Component {
         <Tag>已完成</Tag>
       </div>
       <Table columns={columns} dataSource={data} pagination={false} rowKey="id" />
+
+      {/* 新增任务 */}
+      <Modal
+        title="新增任务"
+        // visible={visible}
+        visible={true}
+        onOk={this.handleOK}
+        onCancel={this.handleCancel}
+      >
+        <p>任务描述：</p>
+        <TextArea rows={3} />
+        <p>完成时间：</p>
+        <DatePicker format="YYYY-MM-DD HH:mm:ss" />
+      </Modal>
     </div>
   }
 }
