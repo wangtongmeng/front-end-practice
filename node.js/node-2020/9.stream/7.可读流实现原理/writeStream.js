0ad3a4e3dc610243f917eb2ac11a3976a28c3ea6
littlebirdflying <tongmengwang@163.com>
2021-02-02 18:03:33 +0800

    up

--- node.js/node-2020/9.stream/7.可读流实现原理/writeStream.js
+++ node.js/node-2020/9.stream/7.可读流实现原理/writeStream.js
@@ -17,6 +17,13 @@ class WriteStream extends Writable {
         this.highWaterMark = options.highWaterMark || 16 * 1024
 
         this.open()
+
+        // 要判断是第一次写入 还是第二次写入
+        this.writing = false // 用来描述当前是否有正在写入的操作
+        this.needDrain = false // 默认是否触发drain事件
+        this.len = 0 // 需要统计的长度
+        this.offset = 0 // 每次写入时的偏移量
+        this.cache = [] // 用来实现缓存的
     }
     open() {
         fs.open(this.path, this.flags, (err, fd) => {
