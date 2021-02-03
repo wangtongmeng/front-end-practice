8fe7bf5f5fb4bb21dc18ebf13dfdb4a77d99c886
littlebirdflying <tongmengwang@163.com>
2021-02-03 13:45:24 +0800

    Update 8.LinkList.js

--- node.js/node-2020/9.stream/8.LinkList.js
+++ node.js/node-2020/9.stream/8.LinkList.js
@@ -1,4 +1,3 @@
-
 // 常见的数据结构 队列 栈 链表 树
 // 队列 先进先出 对应数组的push shift 例如：事件环
 // 栈 先进后出 对应数组的push pop 例如：方法调用栈、路由切换、浏览器的历史记录(两个栈)
@@ -9,3 +8,29 @@
 
 
 // 函数声明时定了作用域 运行时产生执行上下文
+
+class Node {
+    constructor(element, next) {
+        this.element = element
+        this.next = next
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+    // 增加节点
+    add() {}
+    // 删除节点
+    remove() {}
+    // 获取节点
+    getNode() {}
+    // 链表的总个数
+    size() {}
+}
+let ll = new LinkedList()
+ll.add(0, 100)
+ll.add(0, 200)
+ll.add(300) // 200 100 300
