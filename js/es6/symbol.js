5a739a60eba78152cc2cb21f59fd0858c31066b1
littlebirdflying <tongmengwang@163.com>
2021-02-25 18:02:15 +0800

    Update symbol.js

--- js/es6/symbol.js
+++ js/es6/symbol.js
@@ -1,6 +1,7 @@
 // Symbol 基本数据类型(基本数据类型：string number boolean undefined null symbol bigint)
 // Symbol 独一无二的的类型 声明一个Symbol 声明一个带表示的Symbol
-// 用Symbol做对象的属性  如何遍历对象的Symbol属性
+// 用Symbol做对象的属性
+// 如何遍历对象的Symbol属性 Symbol属性默认是不能枚举
 
 // let s1 = Symbol()
 // let s2 = Symbol()
@@ -17,3 +18,4 @@
 // }
 // console.log(obj) // { name: 'tm', [Symbol(tm)]: 'ok' }
 // console.log(obj[s1]) // ok
+
