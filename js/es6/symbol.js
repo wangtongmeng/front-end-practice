2994c880402ea94bfd000c8257edc20c4b1743bc
littlebirdflying <tongmengwang@163.com>
2021-02-25 12:56:59 +0800

    Create symbol.js

---
+++ js/es6/symbol.js
@@ -0,0 +1,19 @@
+// Symbol 基本数据类型(基本数据类型：string number boolean undefined null symbol bigint)
+// Symbol 独一无二的的类型 声明一个Symbol 声明一个带表示的Symbol
+// 用Symbol做对象的属性  如何遍历对象的Symbol属性
+
+// let s1 = Symbol()
+// let s2 = Symbol()
+// console.log(s1 === s2) // false
+
+// let s1 = Symbol('tm')
+// let s2 = Symbol('tm')
+// console.log(s1 === s2) // false
+
+// let s1 = Symbol('tm')
+// let obj = {
+//     name: 'tm',
+//     [s1]: 'ok'
+// }
+// console.log(obj) // { name: 'tm', [Symbol(tm)]: 'ok' }
+// console.log(obj[s1]) // ok
