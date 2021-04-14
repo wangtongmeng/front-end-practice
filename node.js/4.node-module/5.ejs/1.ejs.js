f016ed233339b830bb53b9d6ea043cf8ef2edc7a
wangtongmeng <tongmengwang@163.com>
2021-04-14 17:57:23 +0800

    up

--- node.js/4.node-module/5.ejs/1.ejs.js
+++ node.js/4.node-module/5.ejs/1.ejs.js
@@ -14,25 +14,25 @@ const read = util.promisify(fs.readFile);
 //     }
 // };
 
-// let ejs = {
-//     async renderFile(filename, options) {
-//         let content = await read(filename, 'utf8');
-//         content = content.replace(/<%=(.+?)%>/g, function() { // .+ 后面加? => 非贪婪
-//             return '${'+arguments[1]+'}' // 获取对应的内容做这件事  arguments[1] ()中的内容
-//         });
-//         let head = 'let str = "";\nwith(obj){\n str+=`';
-//         let body = content = content.replace(/<%(.+?)%>/g, function() {
-//             return '`\n' + arguments[1] + '\nstr+=`'
-//         });
-//         let tail = '`} return str';
-//         let fn = new Function('obj',head + body + tail);
-//         return fn(options);
-//     }
-// };
+let ejs = {
+    async renderFile(filename, options) {
+        let content = await read(filename, 'utf8');
+        content = content.replace(/<%=(.+?)%>/g, function() { // .+ 后面加? => 非贪婪
+            return '${'+arguments[1]+'}' // 获取对应的内容做这件事  arguments[1] ()中的内容
+        });
+        let head = 'let str = "";\nwith(obj){\n str+=`';
+        let body = content = content.replace(/<%(.+?)%>/g, function() {
+            return '`\n' + arguments[1] + '\nstr+=`'
+        });
+        let tail = '`} return str';
+        let fn = new Function('obj',head + body + tail);
+        return fn(options);
+    }
+};
 (async function() {
-    let r = await ejs.renderFile('template.html', { name: 'zhangsan', age: 18 })
+    // let r = await ejs.renderFile('template.html', { name: 'zhangsan', age: 18 })
     // 复杂的情况
-    // let r = await ejs.renderFile('template.html', { arr: [1, 2, 3] })
+    let r = await ejs.renderFile('template.html', { arr: [1, 2, 3] })
     console.log(r);
 })();
 
---
+++ node.js/4.node-module/6.events/index.js
@@ -0,0 +1,30 @@
+const EventEmitter = require('events')
+const util = require('util')
+
+function Girl() {}
+util.inherits(Girl, EventEmitter) // 原型继承 需要通过实例来调用继承的方法 
+
+
+let girl = new Girl()
+const cry = () => {console.log('哭')}
+const eat = () => {console.log('吃')}
+const walking = () => {console.log('逛街')}
+girl.on('失恋', cry)
+girl.on('失恋', eat)
+girl.once('失恋', walking)
+
+girl.emit('失恋') // 哭 吃 逛街
+console.log('------------------------')
+girl.emit('失恋') // 哭 吃
+console.log('------------------------')
+girl.off('失恋', cry)
+girl.emit('失恋') // 吃
+
+
+
+
+// 发布订阅模式 redux vue express koa webpack
+// 订阅一次
+// 订阅方法 
+// 发布方法
+// 取消订阅
